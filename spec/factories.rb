918bc6a4feaad9cec2955e5a1cfe26455ad9b820
edgarrambo <ragde956@gmail.com>
2020-01-12 22:14:37 +0000

    added html to grammable

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
+  factory :comment do
+    
+  end
+
   factory :user do
     sequence :email do |n|
        "dummyEmail#{n}@gmail.com"
@@ -8,6 +12,9 @@ FactoryBot.define do
   end
   factory :gram do
     message { "hello" }
+    picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png').to_s, 'image/png') }
+
+
     association :user
   end
   
